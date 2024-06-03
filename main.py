976924de9aaaf0ab04deef1ce18615b763a301d5
NeonicI <fneonic@gmail.com>
2024-06-03 15:20:27 -0500

    Set up chat room app with Gunicorn for production

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
 @app.route('/')
@@ -10,9 +10,9 @@ def index():
     return render_template('index.html')
 
 @socketio.on('message')
-def handle_message(msg):
+def handleMessage(msg):
     print('Message: ' + msg)
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True, port=10000)
+    socketio.run(app, host='0.0.0.0', port=10000)
