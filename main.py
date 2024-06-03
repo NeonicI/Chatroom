d09cc0b550006be843c9c4ffea6f6ac2f65a84d8
NeonicI <fneonic@gmail.com>
2024-06-03 15:15:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True, port=10000)
